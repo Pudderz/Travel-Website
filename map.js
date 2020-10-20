cffc7a5f07ca24edd9dc30b1bb9247de128b2c55
Pudderz <mpudney6@gmail.com>
2020-10-20 18:16:13 +0100

    Update map.js
    added event listener on drag that moves the markers when they are too far away from center of the map.

--- map.js
+++ map.js
@@ -5,25 +5,8 @@ const tileUrl = `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`
 const tiles = L.tileLayer(tileUrl,{attribution})
 tiles.addTo(mymap)
 
-mymap.addControl(new L.Control.SearchMarker({toggleFullscreen: true}));
+//mymap.addControl(new L.Control.SearchMarker({toggleFullscreen: true}));
  mymap.addControl(new L.Control.Fullscreen());
-
-// // `fullscreenchange` Event that's fired when entering or exiting fullscreen.
-mymap.on('fullscreenchange', function () {
-    console.log('test')
-    if (mymap.isFullscreen()) {
-        console.log('entered fullscreen');
-        mymap.toggleShowSearchMarker();
-        mymap.setZoom(2)
-        mymap.fitWorld();
-        
-    } else {
-        mymap.toggleShowSearchMarker();
-        console.log('exited fullscreen');
-        mymap.setView([30,0],0)
-    }
-});
-
 
 
 listOfLocations=[
@@ -61,50 +44,99 @@ listOfLocations=[
     }
 ]
 
-marker={}
+markers=[]
 
+
+const detailedRun = (()=>{
+    listOfLocations.forEach((location)=>{
+        const length = markers.push(L.marker(location.cords, {name: location.name}).addTo(mymap));
+        const index = length -1;
+        console.log(markers[index]);
+        L.DomUtil.addClass(markers[index]._icon, location.name);
+        L.DomUtil.addClass(markers[index]._icon, 'fullscreenMarker');
+        const item = document.querySelector(`#${location.name}Trip`);
+        if(item){
+            var clone = item.cloneNode(true)
+            markers[index].bindPopup(clone,{ maxWidth: 300, minWidth: 200, autoPan: false, className:"fullscreenContent"});
+        }
+        //L.DomUtil.addClass(markers[index]._popup, 'fullscreenContent');
+    })
+})();
 const test = (()=>{
     listOfLocations.forEach((location)=>{
-        marker[location.name] = L.marker(location.cords).addTo(mymap);
-        console.log(marker[location.name]);
-        L.DomUtil.addClass(marker[location.name]._icon, location.name);
-        L.DomUtil.addClass(marker[location.name]._icon, 'normal');
-        marker[location.name].bindPopup(`${location.name}`,{closeButton: false});
-        marker[location.name].on('mouseover', function (e) {
+        const index = markers.push(L.marker(location.cords, {name: location.name}).addTo(mymap)) -1;
+        L.DomUtil.addClass(markers[index]._icon, 'normalMarker');
+        markers[index].bindPopup(`${location.name}`,{closeButton: false});
+        markers[index].on('mouseover', function (e) {
             this.openPopup();
         });
-        marker[location.name].on('mouseout', function (e) {
+        markers[index].on('mouseout', function (e) {
             this.closePopup();
         });
-        marker[location.name].on('click', function(ev) {
+        markers[index].on('click', function(ev) {
             const yOffset = -300; 
             const element = document.getElementById(`${location.name}Trip`);
             const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-            
             window.scrollTo({top: y});
         });
-        
-        
     })
 })();
 
-//add filter
-function checkMap(e){
-    let places = document.querySelectorAll('.leaflet-marker-icon');
-    const shadows = document.querySelector('.leaflet-shadow-pane');
-    if(e.target.value === ''){
-        shadows.style.display = "block";
-    }else{
-        shadows.style.display = "none";
-        
-    }     
-   const value = e.target.value.toLowerCase();
-        places.forEach(place=>{
-            if(place.classList[3].includes(value)){
-                place.style.display = "block";
-            }else{
-                place.style.display = "none";
-            }
-        })
-}
+layerGroup = L.layerGroup(markers);
+const searchController = new L.Control.SearchMarker({layer:layerGroup});
+mymap.addControl(searchController);
+searchController.startSearch();
 
+const fullscreenMarkers = document.querySelectorAll('.fullscreenMarker');
+const minimizedMarkers = document.querySelectorAll('.normalMarker')
+
+
+// // `fullscreenchange` Event that's fired when entering or exiting fullscreen.
+mymap.on('fullscreenchange', function () {
+    console.log('test')
+    if (mymap.isFullscreen()) {
+        console.log('entered fullscreen');
+        fullscreenMarkers.forEach(el=>el.style.display = 'block');
+        minimizedMarkers.forEach(el=>el.style.display = 'none');
+//        mymap.toggleShowSearchMarker();
+        mymap.setZoom(2)
+        mymap.fitWorld();
+        
+    } else {
+        fullscreenMarkers.forEach(el=>el.style.display = 'none');
+        minimizedMarkers.forEach(el=>el.style.display = 'block');
+        mymap.closePopup();
+//        mymap.toggleShowSearchMarker();
+        console.log('exited fullscreen');
+        mymap.setView([30,0],0)
+    }
+});
+mymap.on('popupopen', ()=>{
+    console.log('popupOpen');
+    M.AutoInit();
+})
+
+
+// this.mapInstance.on('editable:drawing:end', ({ layer }) => {
+//     if (layer instanceof L.Marker) {
+//       console.log(layer.getLatLng())
+//     } else if (layer instanceof L.Polyline) {
+//       console.log(layer.getLatLngs())
+//     }
+//   })
+
+
+mymap.on('drag', ()=>{
+    let {lng} = mymap.getCenter();
+    let x = Math.trunc(lng);
+    markers.forEach((marker)=>{
+        const {lat:markerLat, lng:markerLng}= marker.getLatLng();
+        if(x - markerLng > 180){
+            var newLatLng = new L.LatLng( markerLat, markerLng+360);
+            marker.setLatLng(newLatLng); 
+        }else if(lng - markerLng < -180){
+            var newLatLng = new L.LatLng(markerLat, markerLng-360);
+            marker.setLatLng(newLatLng); 
+        }
+    })
+})
